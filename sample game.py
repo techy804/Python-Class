c638e746ecd4bb2c1b6e60fde8d799a3223288fe
techy804 <38736298+techy804@users.noreply.github.com>
2019-05-16 11:35:01 -0400

    Create sample game.py

---
+++ sample game.py
@@ -0,0 +1,41 @@
+'''A1=input("A1")
+A2=input("A2")
+A3=input("A3")
+B1=input("B1")
+B2=input("B2")
+B3=input("B3")
+C1=input("C1")
+C2=input("C2")
+C3=input("C3")
+print(A1+"|"+A2+"|"+A3+"\n----------\n"+B1+"|"+B2+"|"+B3+"\n------------\n"+C1+"|"+C2+"|"+C3)'''
+score=0
+print("Type ''a'', ''b'', or ''c''(Case sensitive) to go to a game: ")
+d=input()
+if(d=a):
+  print("Thanos Snap")
+  f=input("Type anything to see if you'll be snapped out of existance: "))
+  g=range(0,1)
+  if(g=0):print("Thanos will spare you") score=score+1;
+    elif(g=1):print("Thanos will snap you out of existince")
+  print(score)
+elif(d=b):
+  print("Lottery Ticket")
+  h=input("Type anything to start")
+  s=input(int("Enter a Wager: "))
+  score=score
+  i=input(int("Enter a Number between 1-20: "))
+  j=input(int("Enter a Number between 1-20: "))
+  k=input(int("Enter a Number between 1-20: "))
+  l=input(int("Enter a Number between 1-20: "))
+  m=input(int("Enter a Number between 1-20: "))
+  n=range(1,20)
+  o=range(1,20)
+  p=range(1,20)
+  q=range(1,20)
+  r=range(1,20)
+  if(i=n): score=score+wager
+  if(j=o): score=score+wager
+  if(k=p): score=score+wager
+  if(l=q): score=score+wager
+  if(m=r): score=score+wager
+  print(score)
